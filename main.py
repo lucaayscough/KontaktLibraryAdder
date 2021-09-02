a4ee47ca7b3aa7b4163985191a05a5efd95c3d48
mishell <lucaayscough@gmail.com>
2021-09-02 13:20:38 +0200

    Written conversion script.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import os
-import tkinter as tk
+from tkinter import filedialog as fd
+
+
+filename = fd.askopenfilename()
+library_name = filename[filename.rfind("/") + 1 : filename.find(".")]
+
+contents = open(filename, "r", encoding="iso-8859-1").read()
+contents = contents[contents.find("<?xml") : contents.find("</ProductHints>") + len("</ProductHints>")]
+
+xml = open("/Library/Application Support/Native Instruments/Service Center/" + library_name + ".xml", "w")
+xml.write(contents)
