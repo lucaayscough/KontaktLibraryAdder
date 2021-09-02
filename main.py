f1984ea1dab71c24bc37bb8400a43520c0cb2092
Mishell <lucaayscough@gmail.com>
2021-09-02 16:41:31 +0200

    Changes

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
-from tkinter import filedialog as fd
 
+lib_dir = "/Volumes/Extreme SSD/Librerie/"
 
-filename = fd.askopenfilename()
-library_name = filename[filename.rfind("/") + 1 : filename.find(".")]
+for dir in os.listdir(lib_dir):
+    if os.path.isdir(lib_dir + "/" + dir) and dir[0] != ".":
+        lib_files = os.listdir(lib_dir + "/" + dir)
+        for lib_file in lib_files:
+            if lib_file.find(".nicnt") != -1:
+                filename = lib_dir + "/" + lib_file
+                library_name = filename[filename.rfind("/") + 1 : filename.find(".")]
 
-contents = open(filename, "r", encoding="iso-8859-1").read()
-contents = contents[contents.find("<?xml") : contents.find("</ProductHints>") + len("</ProductHints>")]
+                contents = open(filename, "r", encoding="iso-8859-1").read()
+                contents = contents[contents.find("<?xml") : contents.find("</ProductHints>") + len("</ProductHints>")]
 
-xml = open("/Library/Application Support/Native Instruments/Service Center/" + library_name + ".xml", "w")
-xml.write(contents)
+                xml = open("/Library/Application Support/Native Instruments/Service Center/" + library_name + ".xml", "w")
+                xml.write(contents)
